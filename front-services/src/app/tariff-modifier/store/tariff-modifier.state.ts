9d42eb466ac6ace4d56d5f0fb2e4fd0dc1eb4814
Kirill Sklyarov <kirill_sklyarov@epam.com>
2021-02-04 16:55:08 +0300

    remove store from services app

--- front-services/src/app/tariff-modifier/store/tariff-modifier.state.ts
+++
@@ -1,9 +0,0 @@
-import { TariffModifier } from '../../models/tariff-modifier';
-
-export interface TariffModifierState {
-  allTariffModifierList: TariffModifier[] | null;
-}
-
-export const tariffModifierInitialState: TariffModifierState = {
-  allTariffModifierList: null,
-};
